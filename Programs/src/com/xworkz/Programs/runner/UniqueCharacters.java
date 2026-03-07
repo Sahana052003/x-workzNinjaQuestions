7c3c93f11f07c11794eeaaaee3b0f07224dd49a5
Sahana052003 <sahananreddy52003@gmail.com>
2026-03-07 21:15:51 +0530

    Programs

--- Programs/src/com/xworkz/Programs/runner/UniqueCharacters.java
+++ Programs/src/com/xworkz/Programs/runner/UniqueCharacters.java
@@ -25,22 +25,49 @@ public class UniqueCharacters {
 //
 
 
-    public static void main(String[] args) {
-        String name = "Programming";
+//    public static void main(String[] args) {
+//        String name = "Programming";
+//        name = name.toLowerCase();
+//
+//        System.out.println("Unique Characters are : ");
+//        for (int i=0;i<name.length();i++){
+//            char ch=name.charAt(i);
+//            int count=0;
+//            for (int j=0;j<name.length();j++){
+//                if (ch==name.charAt(j)){
+//                    count++;
+//                }
+//            }
+//            if (count==1){
+//                System.out.print(ch + " ");
+//            }
+//        }
+//    }
+//}
+
+
+    public UniqueCharacters(String name) {
         name = name.toLowerCase();
 
-        System.out.println("Unique Characters are : ");
-        for (int i=0;i<name.length();i++){
-            char ch=name.charAt(i);
-            int count=0;
-            for (int j=0;j<name.length();j++){
-                if (ch==name.charAt(j)){
+        System.out.println("Unique characters are:");
+
+        for (int i = 0; i < name.length(); i++) {
+            char current = name.charAt(i);
+            int count = 0;
+            for (int j = 0; j < name.length(); j++) {
+                if (current == name.charAt(j)) {
                     count++;
                 }
             }
-            if (count==1){
-                System.out.print(ch + " ");
+            if (count == 1) {
+                System.out.print(current + " ");
             }
+
+
         }
+    }
+
+    public static void main(String[] args) {
+UniqueCharacters uniqueCharacters=new UniqueCharacters("Programming");
     }
 }
